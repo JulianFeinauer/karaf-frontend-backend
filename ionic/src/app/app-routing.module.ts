9d7b745703521e3283dad0f837d13dde158e0772
Niklas Merz <niklasmerz@linux.com>
2019-12-30 11:19:25 +0100

    Use Angular baseUrl instead for ionic path
    Opens #3

--- ionic/src/app/app-routing.module.ts
+++ ionic/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'ionic', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
-    path: 'ionic/detail/:id',
+    path: 'detail/:id',
     loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
   },
 ];
