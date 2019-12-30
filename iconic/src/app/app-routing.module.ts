648d5755a58bf78f32f1bb8b2444e4ca08c80021
Julian Feinauer <j.feinauer@pragmaticminds.de>
2019-12-30 10:32:27 +0100

    Refacgtored to make two frontend packages, angular and iconic and get everything to run (especially iconic)

--- iconic/src/app/app-routing.module.ts
+++ iconic/src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'iconic', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
-    path: 'detail/:id',
+    path: 'iconic/detail/:id',
     loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
   },
 ];
